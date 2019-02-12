e54d23d4e2b47e078fbd55c628fc63934bf45158
David y Ruben Beltran Sanchez <46919377+drepic33@users.noreply.github.com>
2019-02-12 13:15:27 +0100

    Add files via upload

---
+++ Fase 1/src/test/java/es/urjc/Speedy/SpeedyCarApplicationTests.java
@@ -0,0 +1,17 @@
+package es.urjc.Speedy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpeedyCarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
